ab66df9dfe1da0c9a98fcac1078e76012aa7f72c
ZEROPOINT <wegj1@hotmail.com>
2023-11-05 18:55:14 -0700

    Update start_7zip.bat

--- start_7zip.bat
+++ start_7zip.bat
@@ -1,8 +1,7 @@
-@echo off
-echo  ___       _   _   _   _   _   _   _  
-echo |_7_|     |P|o|r|t|a|b|l|e| 7 | Z |i|p|  
-echo                                   
+@echo off                   
+
 start 7zFM.exe
+
 pause
 echo Press any key to close this window.
 pause >nul
