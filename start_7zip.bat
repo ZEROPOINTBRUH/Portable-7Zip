a5514eccde1047ba1e0c323e17aebe84fe594f7f
ZEROPOINT <wegj1@hotmail.com>
2023-11-05 18:54:15 -0700

    Create start_7zip.bat

---
+++ start_7zip.bat
@@ -0,0 +1,8 @@
+@echo off
+echo  ___       _   _   _   _   _   _   _  
+echo |_7_|     |P|o|r|t|a|b|l|e| 7 | Z |i|p|  
+echo                                   
+start 7zFM.exe
+pause
+echo Press any key to close this window.
+pause >nul
